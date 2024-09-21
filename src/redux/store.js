68d3f3e92a8e6e70998c326795e666d7ad060271
Yana17-ratsa <ratsayana13@gmail.com>
2024-09-21 05:29:15 -0700

    processing to edid

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
+import { persistStore, persistReducer, FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER, } from "redux-persist";
+    import storage from "redux-persist/lib/storage";
 
 
+    const persistedContactsReader = persistReducer({
+        key: "user-contact",
+        storage,
+        whitelist: ["id", "name", "number"]
+    }, contactsReducer);
+
+    const persistedFiltersReducer = persistReducer({
+        key: "user-filter",
+        storage,
+        whitelist: ["name"]
+    }, filtersReducer);
 
 export const store = configureStore({
     reducer: {
-        contacts: contactsReducer,
-        filters: filtersReducer,
-    }
+        contacts: persistedContactsReader,
+        filters: persistedFiltersReducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        }),
 });
+
+export const persistor = persistStore(store);
